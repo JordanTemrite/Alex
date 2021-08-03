ed551ff1597a2ccef27b83614bf32d825140a116
StakeHub <83736116+StakeHubFinance@users.noreply.github.com>
2021-08-02 23:11:55 -0400

    Update 4.sol

--- Contracts/4.sol
+++ Contracts/4.sol
@@ -1050,10 +1050,13 @@ abstract contract Ownable is Context {
 
 contract Token4 is ERC20, Ownable {
     using SafeMath for uint256;
-    using SafeERC20 for IERC20; 
+    using SafeERC20 for IERC20;
     
-    constructor(address _owner, uint256 _supply, string memory _name, string memory _symbol, uint256 _decimals) ERC20(_name, _symbol, _decimals) {
-        _mint(_owner, _supply);
+    uint256 internal supply;
+    
+    constructor(address _owner, string memory _name, string memory _symbol, uint256 _decimals) ERC20(_name, _symbol, _decimals) {
+        supply = 10000 * (1*(10**_decimals));
+        _mint(_owner, supply);
     }
     
 }

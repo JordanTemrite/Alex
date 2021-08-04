05f0a7213508a1d1ab1aa500e8982e8108e37501
StakeHub <83736116+StakeHubFinance@users.noreply.github.com>
2021-08-03 20:43:49 -0400

    Update VS-24.sol

--- Contracts/VariableSupply/VS-24.sol
+++ Contracts/VariableSupply/VS-24.sol
@@ -1055,7 +1055,9 @@ contract Token24 is ERC20, Ownable {
     mapping(address => uint256) internal amountMinted;
     
     constructor(address _owner, uint256 _supply, string memory _name, string memory _symbol) ERC20(_name, _symbol) {
-        _mint(_owner, _supply);
+        uint256 supply;
+        supply = _supply * 1e18;
+        _mint(_owner, supply);
     }
     
     function setMintPermissionAmount(address _minter, uint256 _amount) public onlyOwner {
